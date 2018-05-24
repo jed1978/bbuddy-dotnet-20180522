260215da634c607be00f21076eca4654a0f16f0c
Jed Lin <jed1978@gmail.com>
2018-05-24 15:59:37 +0800

    refactor unit test cases

--- GOOS_SampleTests/Controllers/BudgetServiceTests.cs
+++ GOOS_SampleTests/Controllers/BudgetServiceTests.cs
@@ -5,8 +5,6 @@ using GOOS_Sample.Models;
 using GOOS_Sample.Repoitories;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NSubstitute;
-using NSubstitute.Routing.Handlers;
-using OpenQA.Selenium.Remote;
 
 namespace GOOS_SampleTests.Controllers
 {
@@ -21,7 +19,6 @@ namespace GOOS_SampleTests.Controllers
         {
             _repository = Substitute.For<IRepository<BudgetEntity, string>>();
             _target = new BudgetService(_repository);
-            ArrangeMockBudgets();
         }
 
         [TestMethod]
@@ -47,7 +44,7 @@ namespace GOOS_SampleTests.Controllers
                 Amount = 400
             });
             
-            var model = new BudgetViewModel()
+            var model = new BudgetViewModel
             {
                 YearMonth = "2018-08",
                 Amount = 500
@@ -63,6 +60,11 @@ namespace GOOS_SampleTests.Controllers
         [TestMethod]
         public void test_Get_Average_Budget_between_20180415_and_20180515_should_return_620()
         {
+            SetupMock("2018-04", "2018-05", new List<BudgetEntity>
+            {
+                new BudgetEntity{ YearMonth = "2018-04", Amount = 600},
+                new BudgetEntity{ YearMonth = "2018-05", Amount = 620}
+            });
             decimal expected = 620;
             
             var actual = _target.GetAverageBudget(DateTime.Parse("2018-04-15"), DateTime.Parse("2018-05-15"));
@@ -70,9 +72,20 @@ namespace GOOS_SampleTests.Controllers
             Assert.AreEqual(expected, actual);
         }
 
+        private void SetupMock(string start, string end, List<BudgetEntity> entities)
+        {
+            _repository.GetList(start, end).Returns(entities);
+        }
+
         [TestMethod]
         public void test_Get_Average_Budget_between_20180415_and_20180630_should_return_940()
         {
+            
+            SetupMock("2018-04", "2018-06", new List<BudgetEntity>
+            {
+                new BudgetEntity {YearMonth = "2018-04", Amount = 600},
+                new BudgetEntity {YearMonth = "2018-05", Amount = 620}
+            });
             decimal expected = 940;
 
             decimal actual = _target.GetAverageBudget(DateTime.Parse("2018-04-15"), DateTime.Parse("2018-06-30"));
@@ -83,6 +96,11 @@ namespace GOOS_SampleTests.Controllers
         [TestMethod]
         public void test_Get_Average_Budget_between_20180520_and_20180716_should_return_560()
         {
+            SetupMock("2018-05", "2018-07", new List<BudgetEntity>
+            {
+                new BudgetEntity{ YearMonth = "2018-05", Amount = 620},
+                new BudgetEntity{ YearMonth = "2018-07", Amount = 620}
+            });
             decimal expected = 560;
             
             decimal actual = _target.GetAverageBudget(DateTime.Parse("2018-05-20"), DateTime.Parse("2018-07-16"));
@@ -93,6 +111,10 @@ namespace GOOS_SampleTests.Controllers
         [TestMethod]
         public void test_Get_Average_Budget_between_20160115_and_20160213_should_return_251()
         {
+            SetupMock("2016-01", "2016-02", new List<BudgetEntity>
+            {
+                new BudgetEntity{ YearMonth = "2016-02", Amount = 560}
+            });
             decimal expected = 251;
             
             decimal actual = _target.GetAverageBudget(DateTime.Parse("2016-01-15"), DateTime.Parse("2016-02-13"));
@@ -100,30 +122,6 @@ namespace GOOS_SampleTests.Controllers
             Assert.AreEqual(expected, actual);
         }
 
-        private void ArrangeMockBudgets()
-        {
-            _repository.GetList("2018-04", "2018-06").Returns(new List<BudgetEntity>()
-            {
-                new BudgetEntity {YearMonth = "2018-04", Amount = 600},
-                new BudgetEntity {YearMonth = "2018-05", Amount = 620}
-            });
-
-            _repository.GetList("2018-04", "2018-05").Returns(new List<BudgetEntity>()
-            {
-                new BudgetEntity{ YearMonth = "2018-04", Amount = 600},
-                new BudgetEntity{ YearMonth = "2018-05", Amount = 620}
-            });
-
-            _repository.GetList("2018-05", "2018-07").Returns(new List<BudgetEntity>()
-            {
-                new BudgetEntity{ YearMonth = "2018-05", Amount = 620},
-                new BudgetEntity{ YearMonth = "2018-07", Amount = 620}
-            });
-
-            _repository.GetList("2016-01", "2016-02").Returns(new List<BudgetEntity>()
-            {
-                new BudgetEntity{ YearMonth = "2016-02", Amount = 560}
-            });
-        }
+        
     }
 }
